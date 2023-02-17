77619336990ce1931273bbf6f67e1013c78b0f78
Darvand <david.montoya@rootstrap.com>
2023-02-17 14:33:57 -0500

    [85zrmhwk6] Add coverage

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,9 @@ module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
   collectCoverageFrom: ["./src/**"],
   coveragePathIgnorePatterns: ["/infraestructure/models/proto/", "/*.fake.ts", "src/server.ts", "src/init.ts"],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
 };
